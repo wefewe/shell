da0154214ef92d76a4cea9da59c5bbb0816f3b44
MaruKoh <37258122+MaruKoh@users.noreply.github.com>
2019-06-22 15:35:26 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ url='https://raw.githubusercontent.com/MaruKoh'
 if [[ $pro == 1 ]];then
     echo "该项目正在构建中…";fi
 if [[ $pro == 2 ]];then
-	wget -N --no-check-certificate $url/shell/master/sevpn && bash sevpn;fi
+	wget -N --no-check-certificate -q $url/shell/master/sevpn && bash sevpn;fi
 if [[ $pro == 3 ]];then
-    wget -N --no-check-certificate $url/fas-/master/fast.bin && bash fast.bin;fi
+    wget -N --no-check-certificate -q $url/fas-/master/fast.bin && bash fast.bin;fi
 if [[ $pro == 4 ]];then
-    wget -N --no-check-certificate $url/V2Proxy/master/install.sh && bash install.sh;fi
+    wget -N --no-check-certificate -q $url/V2Proxy/master/install.sh && bash install.sh;fi
 exit;0
 rm -rf /root/build.sh
