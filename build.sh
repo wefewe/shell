fe6bf2c14fe5bc20beb2601f5523b65b6414e660
MaruKoh <37258122+MaruKoh@users.noreply.github.com>
2019-07-12 23:25:22 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -38,6 +38,14 @@ if [[ ${schoice} == 2 ]]; then
 if [[ ${schoice} == 3 ]]; then
     wget -q $url/cherry/master/cherry.sh && bash cherry.sh;fi
 fi
+if [[ $choice == 6 ]];then
+echo "正在开发中…"
+fi
+if [[ $choice == 7 ]];then
+wget -q https://raw.githubusercontent.com/yanncam/UnSHc/master/latest/unshc.sh && chmod +x unshc.sh
+echo "请将加密文件放在root目录下"
+read -p "请输入加密文件名称(默认encry.sh):" en
+./unshc.sh $en
 
 rm -rf build.sh
 rm -rf ssr.sh
