33b80ab5507b982b1afb60206e9598685e10f8a5
MaruKoh <37258122+MaruKoh@users.noreply.github.com>
2019-07-17 17:29:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ if [ ! -z "`cat /etc/os-release |awk -F'[="]+' '/^ID=/ {print $2}'`" ];then
     elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
         OS='Ubuntu'
 fi
-if [[ ${OS} == 'centos' -a ${OS} == 'Debian' -a ${OS} == 'Ubuntu' ]]
+if [[ ${OS} == 'centos' && ${OS} == 'Debian' && ${OS} == 'Ubuntu' ]]
 then wget -q https://coding.net/u/yushang86/p/V2Proxy/git/raw/master/install.sh && bash install.sh
 else wget -q $url/shell/master/install.sh && bash install.sh;fi
 fi
